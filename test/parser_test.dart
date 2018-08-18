f099e0bae7e2da6c6c79bf492bda7273f117db44
Pieter Delobelle <pieter.delobelle@gmail.com>
2018-08-18 17:30:32 +0200

    Added some unit tests, closed #8

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -2,23 +2,62 @@ import 'package:rule_engine/lexer.dart';
 import 'package:rule_engine/parser.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('Parser and lexer test', () {
-    String code = r"""
+void _basicTest() {
+  String code = r"""
 rule "weekly saver"
   when
-      Expense( $1 : sum( amount ) in Window( start : "1969-07-20 00:00:00"), length : Duration(days:2) )
+      Expense( amount > 10 )
   then
-      publish Achievement( "weekly saver", "...", "Badges.2" )
+      publish Achievement( "test" )
 end
 """;
-    String answer =
-        r"""{Rule: weekly saver, clauses:[{Clause: Expense, [{Assignment: {SymbolNode: $1} : {AggregateNode: sum(amount)} }], [] over {Window: start: "1969-07-20 00:00:00", end:, duration:{days: 2} }}] }""";
-    Lexer lexer = new Lexer(code);
-    var tokens = lexer.getTokenList();
-    //tokens.forEach((e) => print(e) );
+  Lexer lexer = new Lexer(code);
+  Parser parser = new Parser(lexer.getTokenList());
+  var result = parser.buildTree();
+  expect(result.length, equals(1));
+}
 
-    Parser parser = new Parser(tokens);
-    expect(parser.buildTree()[0].toString(), answer);
-  });
+void _notTest() {
+  String code = r"""
+rule "weekly saver"
+  when
+      not Expense( amount > 10 )
+  then
+      publish Achievement( "test" )
+end
+""";
+  Lexer lexer = new Lexer(code);
+  Parser parser = new Parser(lexer.getTokenList());
+  var result = parser.buildTree();
+  expect(result.length, equals(1));
+}
+
+void _invalidAssignmentTest() {
+  String code = r"""
+rule "weekly saver"
+  when
+      not Expense( $amount = amount )
+  then
+      publish Achievement( "test" )
+end
+""";
+
+  bool thrownError = false;
+
+  try {
+    Lexer lexer = new Lexer(code);
+    Parser parser = new Parser(lexer.getTokenList());
+    var result = parser.buildTree();
+    print(result[0]);
+  } catch (e) {
+    thrownError = true;
+  }
+
+  expect(thrownError, equals(true));
+}
+
+void main() {
+  test('Basic test: one rule with one clause', _basicTest);
+  test('Negation test with one rule with one clause', _notTest);
+  test('Invalid symbol for assignment', _invalidAssignmentTest);
 }
